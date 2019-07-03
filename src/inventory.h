cc578b5b5e7e3c6678b6ef6d989e91276d46585c
vaguerant <vaguerant@users.noreply.github.com>
2019-07-03 18:04:52 +1000

    Actually correctly center LevelStats

--- src/inventory.h
+++ src/inventory.h
@@ -2032,7 +2032,7 @@ struct Inventory {
         }
 
         if (page == PAGE_LEVEL_STATS) {
-            showLevelStats(vec2(0, UI::height / 2 - 60));
+            showLevelStats(vec2(0, UI::height / 2 - 75));
             return;
         }
 
